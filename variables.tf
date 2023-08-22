1dfc32f8e500289cc815673637ddd15c1651f2fd
challasilpa <139464088+challasilpa@users.noreply.github.com>
2023-08-22 19:13:49 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,4 @@
  resource "local_file" "my_pet" {
     filename = var.filename
     content = var.content
-
     }
-variable "filename"{
-    default ="pets.txt"
-    type = string
-}
-variable "content"{
-default = "hello"
-} 
