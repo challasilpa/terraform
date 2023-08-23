4be57990914be8f8a38637bd1259c5dd06f50ef2
challasilpa <139464088+challasilpa@users.noreply.github.com>
2023-08-23 17:54:30 +0530

    Update terraform2.tf

--- terraform2.tf
+++ terraform2.tf
@@ -1,4 +1,9 @@
 resource "local_file" "test" {
-  filename = "silpa.txt"
-  content  = "hello"
+  filename = "var.filename"
+  content  = "var.content"
   }
+
+variable "filename" {
+  defualt = "silpa.txt"
+  content = "hello"
+}
