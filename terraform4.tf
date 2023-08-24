8f5ed283da2a1c0802931ef77f0741a38bb7d0df
challasilpa <139464088+challasilpa@users.noreply.github.com>
2023-08-24 18:17:47 +0530

    Update terraform4.tf

--- terraform4.tf
+++ terraform4.tf
@@ -3,11 +3,11 @@ filename = var.filename[count.index]
 content = "I love cats!"
 count = 3
 }
-,
+
 variable "filename" {
 default = [
-"pets.txt"
-"cats.txt"
+"pets.txt",
+"cats.txt",
 "dogs.txt"
 ]
 }
